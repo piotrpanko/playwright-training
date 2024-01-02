6840af74602f93e47548678db83fc2e5fafb10f0
Piotr Pańko <p.panko@outlook.com>
2024-01-02 14:29:12 +0100

    Add missing setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='playwright-training',
+    version='0.1',
+    packages=[''],
+    url='https://www.mrcavior.com',
+    license='',
+    author='Piotr "Cavior" Pańko',
+author_email = 'piotr@mrcavior.com',
+description = 'Collection of playwright tests of various qa-friendly websites'
+)
